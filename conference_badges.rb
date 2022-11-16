5ee85937aa0a9c910741348f67d5e98477f4e57e
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-16 18:51:19 -0500

    commit tree e53edf0ace79ec3af4372b4256ca437765541d91

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(badge)
+  "Hello, my name is #{badge}."
+end
+
+def batch_badge_creator (names)
+  names.each do |name|
+    "Hello, my name is #{name}."
+  end
+  binding.pry
+end
+
+# def assign_rooms ()
